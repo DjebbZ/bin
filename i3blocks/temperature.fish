87efab44d23458b11de429315582434606abd5aa
DjebbZ <khalid.jebbari@gmail.com>
2017-05-02 11:12:32 +0200

    Got something working. Value-based programming FTW :)

--- i3blocks/temperature.fish
+++ i3blocks/temperature.fish
@@ -1,40 +1,22 @@
 #!/usr/bin/env fish
 
-### Multicore temperature script for i3blocks.
-
-# 4 cores temperatures
-set full_text (sensors | grep Core | awk '{print substr($3, 2, length($3)-5)}' | tr '\n' ' ' | sed 's/ /°C /g')
+### Temperature script for i3blocks.
+### The one shipped with i3blocks retrieves the wrong temperature.
 
 # Package temperature
-set short_text (sensors | grep 'Package id' | awk '{print substr($4, 2, length($4)-5)}' | tr '\n' ' ' | sed 's/ /°C  /g' | sed 's/  $//')
-
-# max of 4 cores
-set temps (echo $full_text | sed 's/°C//g' | sed 's/ /\n/g')
-
-# consider 1st as max, and check others to avoid unecessary computations
-set max $temps[1]
-
-if test $temps[2] -ge $temps[1] -a $temps[2] -ge $temps[3] -a $temps[2] -ge $temps[4]
-  set max $temps[2]
-else if test $temps[3] -ge $temps[1] -a $temps[3] -ge $temps[2] -a $temps[3] -ge $temps[4]
-  set max $temps[3]
-else if test $temps[4] -ge $temps[1] -a $temps[4] -ge $temps[2] -a $temps[4] -ge $temps[3]
-  set max $temps[4]
-end
+set value (sensors | grep 'Package id' | awk '{print substr($4, 2, 2)}')
 
 # color and icon depending on thresholds
 set color "#00FF00" # temp < 45
-set full_text " $full_text"
-if test $max -gt 45 -a $max -lt 65
+if test $value -gt 45 -a $value -lt 65
   set color "#FFD700"
-  set full_text " $full_text"
-else if test $max -ge 65
+else if test $value -ge 65
   set color "#FF0000"
-  set full_text " $full_text"
 end
 
+set full_text (string join " " "" $value "°C")
 
 ### Output
-echo $short_text
-echo $short_text
-echo $color
+echo $full_text   # i3bar's full_text
+echo $full_text   # i3bar's short_textr
+echo $color       # i3bar's color
